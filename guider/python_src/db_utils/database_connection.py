3bc348ccf36d439ef0d07c9092801b74a3ca04c0
Paul Piggott <Flying-piggott@hotmail.com>
2019-12-01 13:57:01 -0500

    Working app, with database support, can create users

--- guider/python_src/db_utils/database_connection.py
+++ guider/python_src/db_utils/database_connection.py
@@ -1,13 +1,16 @@
 import sqlite3
-
+from config import DB_CONNECTION
 
 class DatabaseConnection:
+    """
+    Utility class for accessing the website's database
+    """
     def __init__(self):
         self.conn = None
         self.cur = None
 
     def __enter__(self):
-        self.conn = sqlite3.connect('database.db')
+        self.conn = sqlite3.connect(DB_CONNECTION)
         self.cur = self.conn.cursor()
         return self
 
@@ -19,12 +22,27 @@ class DatabaseConnection:
         self.conn.close()
 
     def _do_query(self, sql_q):
+        """
+        Simple function to do a query
+        :param sql_q: a valid SQL query
+        :return:
+        """
         self.cur.execute(sql_q)
 
     def insert(self, sql_q):
+        """
+        Simple function to insert into the database
+        :param sql_q: a valid SQL query
+        :return:
+        """
         self._do_query(sql_q)
         self.conn.commit()
 
     def fetch(self, sql_q):
+        """
+        Simple function to fetch from the database
+        :param sql_q: A valid SQL query
+        :return: the results as a list
+        """
         self._do_query(sql_q)
         return list(self.cur.fetchall())
